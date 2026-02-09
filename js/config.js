15da743b570ef9af1b30c18a5ec73a706b04d150
AutomationFactoryStudio <olgaamatriain@gmail.com>
2026-02-09 02:40:29 +0100

    Update API_BASE URL for production environment
    Apuntamos a: n8n.mamaplus-zentrum.de

--- js/config.js
+++ js/config.js
@@ -2,4 +2,5 @@
  * MamaPlus - Configuracion del entorno
  * Cambiar esta URL cuando se migre a otro servidor
  */
-const API_BASE = "https://tinafactory-n8n.dmxwfe.easypanel.host";
+const API_BASE = "https://n8n.mamaplus-zentrum.de";
+
